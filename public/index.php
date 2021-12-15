90b788e1d847497442c3a99a8359bebf20d73ea8
pimenvibritania <pimenvibritania@gmail.com>
2021-12-15 08:31:42 +0700

    serverless

---
+++ public/index.php
@@ -0,0 +1,29 @@
+<?php
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = AppFactory::create();
+
+$app->addRoutingMiddleware();
+
+$errorMiddleware = $app->addErrorMiddleware(true, true, true);
+
+//Define app routes
+$app->get('/hello/{name}', function (Request $request, Response $response, $args) {
+    var_dump('/');
+   $name = $args['name'];
+   $payload = json_encode([
+       "message"    => "the endpoint was executed",
+       "name"       => $name
+   ]);
+   $response->getBody()->write($payload);
+   return $response
+       ->withHeader('Content-Type', 'application/json')
+       ->withStatus(201);
+});
+
+$app->run();
